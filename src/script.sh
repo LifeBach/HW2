148862abf777748b907c4b356cd03b49908b5f97
LifeBach <834688520@qq.com>
2021-11-04 12:09:43 +0800

    Readme finished

--- src/script.sh
+++ src/script.sh
@@ -1,8 +1,8 @@
-time ./hw2 ../testcases/p2-1.nets ../testcases/p2-1.cells ../output/p2-1.out
-time ./hw2 ../testcases/p2-2.nets ../testcases/p2-2.cells ../output/p2-2.out
-time ./hw2 ../testcases/p2-3.nets ../testcases/p2-3.cells ../output/p2-3.out
-time ./hw2 ../testcases/p2-4.nets ../testcases/p2-4.cells ../output/p2-4.out
-time ./hw2 ../testcases/p2-5.nets ../testcases/p2-5.cells ../output/p2-5.out
+time ../bin/hw2 ../testcases/p2-1.nets ../testcases/p2-1.cells ../output/p2-1.out
+time ../bin/hw2 ../testcases/p2-2.nets ../testcases/p2-2.cells ../output/p2-2.out
+time ../bin/hw2 ../testcases/p2-3.nets ../testcases/p2-3.cells ../output/p2-3.out
+time ../bin/hw2 ../testcases/p2-4.nets ../testcases/p2-4.cells ../output/p2-4.out
+time ../bin/hw2 ../testcases/p2-5.nets ../testcases/p2-5.cells ../output/p2-5.out
 cd ../verifier
 ./script_v.sh
 cd ../src
